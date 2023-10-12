f66c7b63de6f5854654f44043bcaf91774c3b06b
AngelR <angelram1@outlook.com>
2023-10-12 12:52:41 -0600

    Parte 1 12/10/23

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <title>Deportville</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Deportville</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Jugadores</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">El Club</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Afiliacion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Usuario</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<main>
+    <header>
+        <div class="col-md-12">
+            <div class="card card-body bg-secondary text-light">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="public/images/Chivas.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</main>
+
+<script src="public/js/bootstrap.min.js"></script>
+</body>
+</html>
